684653a531e5bf57ae620810eb9f552a99c075a7
shubenwuming <1324868142@qq.com>
2021-11-28 14:30:27 +0800

    9-回文数

--- 9-回文数.js
+++ 9-回文数.js
@@ -4,24 +4,33 @@
   */
 
 // 字符串
-let isPalindrome= function (x) {
-  let strNum = x.toString();
-  let reverseStrNum = '';
-  for (let i = strNum.length - 1; i >= 0; i--) {
-    reverseStrNum += strNum[i];
+const isPalindrome = function(x) {
+  // 负数以及10的倍数一定不是回文数
+  if(x < 0 || x % 10 === 0 && x !== 0) return false
+  // 反转前
+  const reverseBefore = x.toString()
+  // 反转后
+  let reverseAfter = ''
+  // 倒序遍历
+  for(let i = reverseBefore.length - 1; i >= 0; i--) {
+    reverseAfter += reverseBefore[i]
   }
-  return strNum === reverseStrNum;
+  // 返回比较结果
+  return reverseBefore === reverseAfter
 }
 
 // 数字
-var isPalindrome = function(x) {
-  if(x < 0 || x % 10 === 0 && x !==0) return false;
-  let y = 0;
-  while(x > y) {
-    y = y * 10 + x % 10;
-    x = Math.floor(x / 10);
+const isPalindrome = function(x) {
+  // 负数 10的倍数一定不是回文数
+  if(x < 0 || x % 10 === 0 && x !== 0) return false
+  // 前一半，后一半反转对比
+  let halfBefore = x
+  let halfAfter = 0
+  while(halfBefore > halfAfter) {
+    halfAfter = halfAfter * 10 + halfBefore % 10
+    halfBefore = Math.floor(halfBefore / 10)
   }
-  console.log(x, y);
-  return x === y || x === Math.floor(y / 10);
-};
+  // 数字长度为奇数时，去掉中间的那个数再比较
+  return halfBefore === halfAfter || halfBefore === Math.floor(halfAfter / 10)
+}
 
