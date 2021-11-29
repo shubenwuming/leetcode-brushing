a329efac4edd694b5ac8b143ad1f0174800f3abc
shubenwuming <1324868142@qq.com>
2021-11-29 17:27:53 +0800

    160-相交链表修改

--- 160-相交链表/双指针解法.js
+++ 160-相交链表/双指针解法.js
@@ -17,15 +17,19 @@
  * @return {ListNode}
  */
 const getIntersectionNode = function(headA, headB) {
+  // 任意一个链表为空,不可能相交
   if(headA === null || headB === null) return null
 
+  // 创建双指针p、q，分别从不同链表头节点开始，遍历两个链表的所有节点
   let p = headA
   let q = headB
-
+  // p、q不相等未遍历继续的条件，
   while(p !== q) {
+    // 指针p依次遍历了以headA为起点的链表 、 以headB为起点的链表
     p = p === null ? headB : p.next 
+    // 指针q依次遍历了以headB为起点的链表、 以headA为起点的链表
     q = q === null ? headA : q.next 
   }
-
+  // 相交时p为某个节点，不相交时p为null
   return p
 };
