ede31c7f82a1c899bc330281e9069953892ebc90
shubenwuming <1324868142@qq.com>
2022-01-05 22:17:09 +0800

    feat:102-二叉树的层序遍历-迭代法、递归法

---
+++ 102-二叉树的层序遍历/01-递归法.js
@@ -0,0 +1,56 @@
+/**
+ * 题号：102
+ * 题目地址：https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+ */
+
+/**
+ * 思路：递归
+ * 递归函数的意义：
+ * 边界条件：
+ * 递归过程：
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+
+/**
+ * 递归法：
+ * 递归函数的意义：层序遍历以root为根节点的二叉树
+ * 结束条件：root为空
+ * 递归过程：层序遍历root的左子树、层序遍历root的右子树
+ */
+
+ const levelOrder = function (root) {
+  //  层序遍历的结果数组
+  const res = [];
+  // 递归函数
+  const __levelOrder = function (root, level, res) {
+    // 递归结束条件
+    if(root === null) return;
+    
+    // 把每层节点的值放入结果数组中（层数对应数组下标）
+    if(res.length === level) res.push([]);
+    res[level].push(root.val);
+
+    // 层序遍历左子树
+    __levelOrder(root.left, level + 1, res);
+    // 层序遍历右子树
+    __levelOrder(root.right, level + 1, res);
+  }
+  
+  __levelOrder(root, 0, res);
+
+  // 返回结果数组
+  return res;
+}
---
+++ 102-二叉树的层序遍历/02-迭代法.js
@@ -0,0 +1,62 @@
+/**
+ * 题号：102
+ * 题目地址：https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+ */
+
+/**
+ * 思路：递归
+ * 递归函数的意义：
+ * 边界条件：
+ * 递归过程：
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+/**
+ * BFS 指 广度优先搜索
+ * 维护一个队列（存每层的节点）
+ */
+
+const levelOrder = function (root) {
+  // 结构数组
+  const res = [];
+  
+  // 空树,直接返回
+  if(root === null) return res;
+
+  // ********************* 代码到了这里说明是非空树 ***************************
+
+  // 队列维护树的每层节点
+  const queue = [];
+  // 将根节点和他所对应的层数入队列
+  queue.push({node: root, level: 0});
+
+  // 循环队列
+  while(queue.length) {
+    // 出队
+    const {node, level} = queue.shift();
+
+    // 将出队的节点的值存到res中
+    if(res.length === level) res.push([]);
+    res[level].push(node.val);
+    
+    // 若当前节点有左子树,将其入队
+    if(node.left) queue.push({node: node.left, level: level + 1});
+    // 若当前节点有右子树,将其入队
+    if(node.right) queue.push({node: node.right, level: level + 1});
+  }
+
+  // 返回结果数组
+  return res;
+}
